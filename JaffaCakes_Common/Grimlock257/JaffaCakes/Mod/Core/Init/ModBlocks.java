7bec014feca8421c15f63ed3204f5002668eda5d
Grimlock257 <AdamWatsonOA@gmail.com>
2013-06-12 20:27:16 +0100

    Orange needed code for the Orange Tree Orange Item and Block ( The Orange on the Tree ) in this file

--- JaffaCakes_Common/Grimlock257/JaffaCakes/Mod/Core/Init/ModBlocks.java
+++ JaffaCakes_Common/Grimlock257/JaffaCakes/Mod/Core/Init/ModBlocks.java
@@ -4,7 +4,10 @@ import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import Grimlock257.JaffaCakes.Mod.Blocks.OrangeTreeLeaves;
 import Grimlock257.JaffaCakes.Mod.Blocks.OrangeTreeLog;
+import Grimlock257.JaffaCakes.Mod.Blocks.OrangeTreeOrange;
 import Grimlock257.JaffaCakes.Mod.Blocks.OrangeTreeSapling;
+import Grimlock257.JaffaCakes.Mod.Blocks.OrangeTreeWood;
+import Grimlock257.JaffaCakes.Mod.Items.JCItemOrange;
 import Grimlock257.JaffaCakes.Mod.Lib.BlockIDs;
 import Grimlock257.JaffaCakes.Mod.Lib.Strings;
 import cpw.mods.fml.common.registry.GameRegistry;
@@ -24,22 +27,25 @@ public class ModBlocks {
     public static Block blockOrangeTreeSapling;
     public static Block blockOrangeTreeLog;
     public static Block blockOrangeTreeWood;
-    // ID 12 Is Log bottom and top, ID 11 is Log sides. Same with leaves, 14 is Transparent(Fancy) and 15 is Non-Transparent(Fast)
+    /** ID 12 Is Log bottom and top, ID 11 is Log sides. Same with leaves, 14 is Transparent(Fancy) and 15 is Non-Transparent(Fast) */
     public static Block blockOrangeTreeLeaves;
+    /** blockOrangeTreeOrange is the actual orange block on the tree leaves */
+    public static Block blockOrangeTreeOrange;
 
     public static void init() {
         // Add Blocks
         blockOrangeTreeSapling = new OrangeTreeSapling(BlockIDs.ORANGE_TREE_SAPLING, 10);
         blockOrangeTreeLog = new OrangeTreeLog(BlockIDs.ORANGE_TREE_LOG, 11, Material.wood);
-        // TODO: Add class for OrangeTreeWood
-        blockOrangeTreeWood = new OrangeTreeLog(BlockIDs.ORANGE_TREE_WOOD, 13, Material.wood);
-        blockOrangeTreeLeaves = new OrangeTreeLeaves(BlockIDs.ORANGE_TREE_LEAVES, 14);
+        blockOrangeTreeWood = new OrangeTreeWood(BlockIDs.ORANGE_TREE_WOOD, 13, Material.wood);
+        blockOrangeTreeLeaves = new OrangeTreeLeaves(BlockIDs.ORANGE_TREE_LEAVES, 15);
+        blockOrangeTreeOrange = new OrangeTreeOrange(BlockIDs.ORANGE_TREE_ORANGE); // , "orange");
 
         // Register Blocks
         GameRegistry.registerBlock(blockOrangeTreeSapling, Strings.ORANGE_TREE_SAPLING_NAME);
         GameRegistry.registerBlock(blockOrangeTreeLog, Strings.ORANGE_TREE_LOG_NAME);
         GameRegistry.registerBlock(blockOrangeTreeWood, Strings.ORANGE_TREE_WOOD_NAME);
         GameRegistry.registerBlock(blockOrangeTreeLeaves, Strings.ORANGE_TREE_LEAVES_NAME);
+        GameRegistry.registerBlock(blockOrangeTreeOrange, JCItemOrange.class, Strings.ORANGE_TREE_ORANGE_NAME);
 
         // Add Block Names
         LanguageRegistry.addName(blockOrangeTreeSapling, "Orange Tree Sapling");
--- JaffaCakes_Common/Grimlock257/JaffaCakes/Mod/Core/Init/ModItems.java
+++ JaffaCakes_Common/Grimlock257/JaffaCakes/Mod/Core/Init/ModItems.java
@@ -1,6 +1,7 @@
 package Grimlock257.JaffaCakes.Mod.Core.Init;
 
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import Grimlock257.JaffaCakes.Mod.Items.JCItem;
 import Grimlock257.JaffaCakes.Mod.Items.JCItemFood;
 import Grimlock257.JaffaCakes.Mod.Lib.ItemIDs;
@@ -29,9 +30,14 @@ public class ModItems {
     public static Item itemChocolateDrop;
     public static Item itemJaffaCake;
 
+    public static ItemStack itemOrange_0;
+    public static ItemStack itemOrange_1;
+    public static ItemStack itemOrange_2;
+
     public static void init() {
         // Tools
         itemStoneBowl = new JCItem(ItemIDs.STONE_BOWL).setMaxStackSize(16).setIconIndex(0).setItemName("item_StoneBowl");
+
         // Foods
         itemOrange = new JCItemFood(ItemIDs.ORANGE, 4, 3F, false).setIconIndex(1).setItemName("item_Orange");
         itemJaffaDough = new JCItem(ItemIDs.JAFFA_DOUGH).setIconIndex(2).setItemName("item_Jaffa_Dough");
@@ -43,8 +49,14 @@ public class ModItems {
         itemChocolateDrop = new JCItem(ItemIDs.CHOCOLATE_DROP).setIconIndex(8).setItemName("item_Chocolate_Drop");
         itemJaffaCake = new JCItemFood(ItemIDs.JAFFA_CAKE, 6, 8F, false).setIconIndex(9).setItemName("item_Jaffa_Cake");
 
+        // Orange Sub Items ( Meta Data )
+        itemOrange_0 = new ItemStack(ModBlocks.blockOrangeTreeOrange, 16, 0);
+        itemOrange_1 = new ItemStack(ModBlocks.blockOrangeTreeOrange, 16, 1);
+        itemOrange_2 = new ItemStack(ModBlocks.blockOrangeTreeOrange, 16, 2);
+
         // Add Items Names
         LanguageRegistry.addName(itemStoneBowl, "Stone Bowl");
+
         // Add Food Names
         LanguageRegistry.addName(itemOrange, "Orange");
         LanguageRegistry.addName(itemJaffaDough, "Jaffa Dough");
@@ -55,5 +67,11 @@ public class ModItems {
         LanguageRegistry.addName(itemChocolateBar, "Chocolate Bar");
         LanguageRegistry.addName(itemChocolateDrop, "Chocolate Drop");
         LanguageRegistry.addName(itemJaffaCake, "Jaffa Cake");
+
+        // Orange Sub Items
+        LanguageRegistry.addName(itemOrange_0, "Very Unripe Orange");
+        // TODO: Remove these do... as I could't change the textures for inventory.. May add later!
+        // LanguageRegistry.addName(itemOrange_1, "Unripe Orange");
+        // LanguageRegistry.addName(itemOrange_2, "Ripe Orange");
     }
 }
