8eef943929719ae74da9cfb6ca52cc7774db5c64
Grimlock257 <AdamWatsonOA@gmail.com>
2013-06-12 20:24:16 +0100

    Added a Generator Reference file for Generators ( At the moment Orange Trees )

---
+++ JaffaCakes_Common/Grimlock257/JaffaCakes/Mod/Lib/Generator.java
@@ -0,0 +1,14 @@
+package Grimlock257.JaffaCakes.Mod.Lib;
+
+/**
+ * Jaffa Cakes Mod
+ * 
+ * Generator
+ * 
+ * @author Grimlock257
+ * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
+ * 
+ */
+public class Generator {
+    public static int ORANGE_TREE_RARITY = 10;
+}
