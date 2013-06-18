55226e3ff9cb9502acd7b878825735a499512e92
Grimlock257 <AdamWatsonOA@gmail.com>
2013-06-18 07:32:09 +0100

    Removed String items and blocks as textures are no longer in those files

--- JaffaCakes_Common/Grimlock257/JaffaCakes/Mod/Core/Network/Proxy/CommonProxy.java
+++ JaffaCakes_Common/Grimlock257/JaffaCakes/Mod/Core/Network/Proxy/CommonProxy.java
@@ -15,8 +15,6 @@ public class CommonProxy {
     // Items & Blocks
     public static String itemDir = Reference.ITEM_TEXTURE_LOCATION;
     public static String blockDir = Reference.BLOCK_TEXTURE_LOCATION;
-    public static String items = Reference.ITEM_TEXTURE_LOCATION + "items.png";
-    public static String blocks = Reference.BLOCK_TEXTURE_LOCATION + "blocks.png";
 
     // Client Renders
     public void registerRenderers() {
