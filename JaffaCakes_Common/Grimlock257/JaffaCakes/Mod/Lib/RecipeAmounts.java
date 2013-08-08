425ff9df986e146f8f3b8c63855c0eee44e8753d
Grimlock257 <AdamWatsonOA@gmail.com>
2013-08-09 00:47:06 +0100

    Added a lib file for recipe crafting result amounts

---
+++ JaffaCakes_Common/Grimlock257/JaffaCakes/Mod/Lib/RecipeAmounts.java
@@ -0,0 +1,28 @@
+package Grimlock257.JaffaCakes.Mod.Lib;
+
+/**
+ * Jaffa Cakes Mod
+ * 
+ * RecipeAmounts
+ * 
+ * @author Grimlock257
+ * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
+ * 
+ */
+public class RecipeAmounts {
+    // Default Recipe Result Amounts
+    public static int STONE_BOWL_DEFAULT = 1;
+    public static int JAFFA_DOUGH_DEFAULT = 1;
+    public static int RAW_JAM_MIXTURE_DEFAULT = 1;
+    public static int JAM_DISK_DEFAULT = 1;
+    public static int CHOCOLATE_BAR_DEFAULT = 1;
+    public static int JAFFA_CAKE_DEFAULT = 1;
+
+    // Current Recipe Result Amounts
+    public static int STONE_BOWL;
+    public static int JAFFA_DOUGH;
+    public static int RAW_JAM_MIXTURE;
+    public static int JAM_DISK;
+    public static int CHOCOLATE_BAR;
+    public static int JAFFA_CAKE;
+}
