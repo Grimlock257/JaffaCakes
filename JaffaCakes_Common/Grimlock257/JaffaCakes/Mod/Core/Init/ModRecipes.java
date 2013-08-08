09ebb5472395dc85aeddecf3ecf15d29db601191
Grimlock257 <AdamWatsonOA@gmail.com>
2013-08-09 00:48:21 +0100

    Added extra argument so the result for the crafting amounts can be modified from the configuration file

--- JaffaCakes_Common/Grimlock257/JaffaCakes/Mod/Core/Init/ModRecipes.java
+++ JaffaCakes_Common/Grimlock257/JaffaCakes/Mod/Core/Init/ModRecipes.java
@@ -3,6 +3,7 @@ package Grimlock257.JaffaCakes.Mod.Core.Init;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import Grimlock257.JaffaCakes.Mod.Lib.RecipeAmounts;
 import cpw.mods.fml.common.registry.GameRegistry;
 
 /**
@@ -17,14 +18,14 @@ import cpw.mods.fml.common.registry.GameRegistry;
 public class ModRecipes {
     public static void init() {
         // Add Structured Recipes
-        GameRegistry.addRecipe(new ItemStack(ModItems.itemStoneBowl), "S S", " S ", 'S', Block.stone);
-        GameRegistry.addRecipe(new ItemStack(ModItems.itemJaffaDough), "WMW", 'M', Item.bucketMilk, 'W', Item.wheat);
-        GameRegistry.addRecipe(new ItemStack(ModItems.itemRawJamMixture), "WOS", " B ", 'W', Item.bucketWater, 'O', ModItems.itemOrange, 'S', Item.sugar, 'B', ModItems.itemStoneBowl);
-        GameRegistry.addRecipe(new ItemStack(ModItems.itemChocolateBar), " S ", "CCC", " M ", 'S', Item.sugar, 'C', new ItemStack(Item.dyePowder, 1, 3), 'M', Item.bucketMilk);
-        GameRegistry.addRecipe(new ItemStack(ModItems.itemJaffaCake), "D", "J", "B", 'D', ModItems.itemChocolateDrop, 'J', ModItems.itemJamDisk, 'B', ModItems.itemJaffaBase);
+        GameRegistry.addRecipe(new ItemStack(ModItems.itemStoneBowl, RecipeAmounts.STONE_BOWL), "S S", " S ", 'S', Block.stone);
+        GameRegistry.addRecipe(new ItemStack(ModItems.itemJaffaDough, RecipeAmounts.JAFFA_DOUGH), "WMW", 'M', Item.bucketMilk, 'W', Item.wheat);
+        GameRegistry.addRecipe(new ItemStack(ModItems.itemRawJamMixture, RecipeAmounts.RAW_JAM_MIXTURE), "WOS", " B ", 'W', Item.bucketWater, 'O', ModItems.itemOrange, 'S', Item.sugar, 'B', ModItems.itemStoneBowl);
+        GameRegistry.addRecipe(new ItemStack(ModItems.itemChocolateBar, RecipeAmounts.CHOCOLATE_BAR), " S ", "CCC", " M ", 'S', Item.sugar, 'C', new ItemStack(Item.dyePowder, 1, 3), 'M', Item.bucketMilk);
+        GameRegistry.addRecipe(new ItemStack(ModItems.itemJaffaCake, RecipeAmounts.JAFFA_CAKE), "D", "J", "B", 'D', ModItems.itemChocolateDrop, 'J', ModItems.itemJamDisk, 'B', ModItems.itemJaffaBase);
 
         // Add Shapeless
-        GameRegistry.addShapelessRecipe(new ItemStack(ModItems.itemJamDisk), ModItems.itemJamMixture);
+        GameRegistry.addShapelessRecipe(new ItemStack(ModItems.itemJamDisk, RecipeAmounts.JAM_DISK), ModItems.itemJamMixture);
         GameRegistry.addShapelessRecipe(new ItemStack(ModItems.itemChocolateDrop, 9), ModItems.itemChocolateBar);
 
         // Add Smelting
