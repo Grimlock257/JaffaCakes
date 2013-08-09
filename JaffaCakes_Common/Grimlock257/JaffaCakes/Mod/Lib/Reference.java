07872fe8fd8c5dfd0789f4e0e479d1ae160dd602
Grimlock257 <AdamWatsonOA@gmail.com>
2013-08-09 13:52:33 +0100

    Updated version & added TEXTURE_FOLDER_NAME

--- JaffaCakes_Common/Grimlock257/JaffaCakes/Mod/Lib/Reference.java
+++ JaffaCakes_Common/Grimlock257/JaffaCakes/Mod/Lib/Reference.java
@@ -11,14 +11,14 @@ package Grimlock257.JaffaCakes.Mod.Lib;
  */
 public class Reference {
     // Debug mode - Used when making/editing the mod, set to false before compiling!
-    public static boolean debug = false;
+    public static boolean debug = true;
 
     // Mod Details, Defined here to make it easier when upgrading
     public static final String AUTHOR = "Grimlock257";
     public static final String WEBSITE = "http://www.minecraftforum.net/user/1505421-grimlock257/";
     public static final String MODID = "GrimlocksJaffaCakes";
     public static final String NAME = "Jaffa Cakes";
-    public static final String VERSION = "1.2";
+    public static final String VERSION = "2.0";
     public static final String CHANNELS = MODID;
     public static final String CONFIGFOLDER = "GrimlocksMods";
 
@@ -34,4 +34,5 @@ public class Reference {
     public static final String BASE_TEXTURE_LOCATION = "/mods/GrimlocksJaffaCakes/textures/";
     public static final String ITEM_TEXTURE_LOCATION = "/mods/GrimlocksJaffaCakes/textures/items/";
     public static final String BLOCK_TEXTURE_LOCATION = "/mods/GrimlocksJaffaCakes/textures/blocks/";
+    public static final String TEXTURE_FOLDER_NAME = "grimlocksjaffacakes";
 }
